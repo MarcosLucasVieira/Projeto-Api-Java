bbe38b639954effcdd75191de0cdef1e073e08d2
Marcos Lucas Vieira <102480205+MarcosLucasVieira@users.noreply.github.com>
2024-06-15 21:14:50 -0300

    implementando banco de dados com populado em sql

--- api-de-teste/banck-end/projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ api-de-teste/banck-end/projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,13 +1,23 @@
 package br.com.criandoapi.projeto.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.criandoapi.projeto.DAO.IUsuario;
+import br.com.criandoapi.projeto.model.Usuario;
+
 @RestController
-public class UsuarioController {
+public class UsuarioController  {
+	
+	@Autowired
+	private IUsuario dao;
 	
 	@GetMapping("/usuarios")
-	public String texto  () {
-		return "Acessando a API";
+	public List<Usuario> listaUsuarios () {
+		return (List<Usuario>) dao.findAll();		
 	}
+
 }
