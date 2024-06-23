b63864e1d388cd5f70b3e386303314eb86649c39
Marcos Lucas Vieira <102480205+MarcosLucasVieira@users.noreply.github.com>
2024-06-22 22:36:25 -0300

    adicionando security

--- api-de-teste/banck-end/projeto/src/main/java/br/com/criandoapi/projeto/ProjetoApplication.java
+++ api-de-teste/banck-end/projeto/src/main/java/br/com/criandoapi/projeto/ProjetoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 import br.com.criandoapi.projeto.model.Usuario;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class ProjetoApplication {
 
 	public static void main(String[] args) {
---
+++ api-de-teste/banck-end/projeto/src/main/java/br/com/criandoapi/projeto/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package br.com.criandoapi.projeto.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+@Bean
+public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+    return http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers(HttpMethod.GET, "/usuarios").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+            )
+            .build();
+    }
+}
