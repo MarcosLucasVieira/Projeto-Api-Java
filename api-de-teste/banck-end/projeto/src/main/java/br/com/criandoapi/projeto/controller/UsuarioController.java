f2ea9bf1e7627d0382811eca5ed5baafe2864452
Marcos Lucas Vieira <102480205+MarcosLucasVieira@users.noreply.github.com>
2024-06-22 21:33:34 -0300

    Adicionando melhorias

--- api-de-teste/banck-end/projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ api-de-teste/banck-end/projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,24 +1,29 @@
 package br.com.criandoapi.projeto.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.criandoapi.projeto.model.Usuario;
-import br.com.criandoapi.projeto.repository.IUsuario;
 import br.com.criandoapi.projeto.service.UsuarioService;
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin ("*")
@@ -42,12 +47,12 @@ public class UsuarioController  {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario) {
+	public ResponseEntity<Usuario> criarUsuario(@Valid @RequestBody Usuario usuario) {
 		return ResponseEntity.status(201).body(usuarioService.criaUsuario(usuario));
 	}
 	
 	@PutMapping
-	public ResponseEntity<Usuario> editarUsuario(@RequestBody Usuario usuario) {
+	public ResponseEntity<Usuario> editarUsuario(@Valid @RequestBody Usuario usuario) {
 		return ResponseEntity.status(200).body(usuarioService.editarUsuario(usuario));
 	}
 	
@@ -57,11 +62,27 @@ public class UsuarioController  {
 		return ResponseEntity.status(204).build();
 	}
 	@PostMapping("/login")
-	public ResponseEntity<Usuario> validarSenha(@RequestBody Usuario usuario){
+	public ResponseEntity<Usuario> validarSenha(@Valid @RequestBody Usuario usuario){
 		Boolean valid = usuarioService.validarSenha(usuario);
 		if(!valid) {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 		}
 		return ResponseEntity.status(200).build();
 	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handValidationException(MethodArgumentNotValidException ex){
+		Map<String, String> errors = new HashMap<>();
+		
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String erroMassage = error.getDefaultMessage();
+			errors.put(fieldName, erroMassage);
+		});
+		
+		return errors;
+	}
+	
+	
 }
--- api-de-teste/banck-end/projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
+++ api-de-teste/banck-end/projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
@@ -6,9 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 
@@ -21,15 +21,20 @@ public class Usuario {
 	@Column(name="id")
 	private Integer id;
 	
+	@NotBlank(message =" O nome é obrigatório")
 	@Column(name="nome", length= 200, nullable = true)
 	private String nome;
-		
+	
+	@Email(message = "Insira um Email Válido")
+	@NotBlank(message =" O email é obrigatório")
 	@Column(name="email", length= 50, nullable = true)
 	private String email;
 	
+	@NotBlank(message =" A senha é obrigatória")
 	@Column(name="senha", columnDefinition ="TEXT", nullable = true)
 	private String senha;
 	
+	@NotBlank(message =" O telefone é obrigatório")
 	@Column(name="telefone", length= 15, nullable = true)
 	private String telefone;
 	
